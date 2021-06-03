402da5996bc46c4264762d0240ec467280103674
Michael Ruiz <22ruiz@att.net>
2021-06-03 13:47:51 -0600

    basic UI setup

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <h1>Which type is weak to fire?</h1>
+    <button className="answer">fire</button>
+    <button>grass</button>
+    <button>water</button>
+    <button>fairy</button>
+</body>
+</html>
