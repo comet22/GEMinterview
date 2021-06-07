135141e9c9be654d25fe4cb78b6c465ee0b9b705
Michael Ruiz <22ruiz@att.net>
2021-06-06 21:22:50 -0600

    finished css, comments, and started README.md

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
+// global variables, used to store data from the database
 var response = "";
 var questions = [];
 var answers = [];
 var correctAnswers = [];
 var correct = 0;
 
+// called when an answer is chosen
 function handleClick(chosenAnswer) {
     var question = document.getElementById('q').innerHTML;
     for(var i = 0; i < questions.length; i++) {
+        // used to find the correct question
         if(question == questions[i]) {
-            if(correctAnswers[i][chosenAnswer] == true) {
+            // check if the chosen answer is correct
+            if(correctAnswers[i] == chosenAnswer) {
                     correct++;
             }
 
             if(i + 1 < questions.length) {
+                // if this is not the last question, load the next question
                 document.getElementById('q').innerHTML = questions[i+1];
 
                 for(var j = 0; j < answers[i+1].length; j++) {
                     document.getElementById(j).innerHTML = answers[i+1][j];
                 }
             } else {
+                // if this is the last question, print the users score
                 document.getElementById('q').innerHTML = "You got " + correct + " questions correct, congrats!"
                 document.getElementById('answers').style.display = "none";
             }
@@ -27,6 +33,7 @@ function handleClick(chosenAnswer) {
     }
 }
 
+// collects database info
 function handleLoad() {
     var xhttp;
     xhttp = new XMLHttpRequest();
@@ -39,22 +46,17 @@ function handleLoad() {
     xhttp.send();   
 }
 
+// when start is pressed, this function converts the response received into a format the code can use
 function startquiz() {
     var data = String(response).split(' ');
     for(var i = 0; i < data.length-1; i+=6) {
+        // questions don't have spaces in the database to allow the split function to function properly, we change it back here and store the question
         questions.push(data[i].replaceAll('_',' '));
         
-        var correct = data[i + 5];
-        var correctRow = [];
-        for(var j = 0; j < 4; j++) {
-            if(correct == j) {
-                correctRow.push(true);
-            } else {
-                correctRow.push(false);
-            }
-        }
-        correctAnswers.push(correctRow);
+        // stores which answer is correct
+        correctAnswers.push(data[i + 5]);
         
+        // stores the answers
         var responseAnswers = [];
         for(var j = 1; j < 5; j++) {
             responseAnswers.push(data[i+j]);
@@ -63,10 +65,11 @@ function startquiz() {
     }
 
     document.getElementById('description').style.display = "none";
-    document.getElementById('start').style.display = "none";
+    document.getElementById('centerStart').style.display = "none";
 
     document.getElementById('answers').style.display = "block";
 
+    // load the first question
     document.getElementById('q').innerHTML = questions[0];
     for(var i = 0; i < answers[0].length; i++) {
         document.getElementById(i).innerHTML = answers[0][i];
--- index.php
+++ index.php
@@ -7,14 +7,15 @@
 <body onload="handleLoad()">
     <h1 id = 'q'>Quiz Time!</h1>
     <p id = 'description'>This quiz is meant to challenge your Pokemon knowledge!  See how many questions you can get right!!!</p>
-    <button id = 'start' onclick="startquiz()">Start!</button>
-
-    <div id="answers" hidden>
-        <button id='0' onclick="handleClick('0')"></button>
-        <button id='1' onclick="handleClick('1')"></button>
-        <button id='2' onclick="handleClick('2')"></button>
-        <button id='3' onclick="handleClick('3')"></button>
+    <div class='centerButton' id='centerStart'>
+        <button id = 'start' onclick="startquiz()">Start!</button>
     </div>
-    <div id="txtHint"></div>
+
+    <div class = 'centerButton' id="answers" hidden>
+        <button class='answer' id='0' onclick="handleClick('0')"></button>
+        <button class='answer' id='1' onclick="handleClick('1')"></button>
+        <button class='answer' id='2' onclick="handleClick('2')"></button>
+        <button class='answer' id='3' onclick="handleClick('3')"></button>
+    </div>
 </body>
 </html>
